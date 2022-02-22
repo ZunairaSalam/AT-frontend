327d65e37476de1a5c9201ab15d06852da9b244f
Zunaira Salam <zunairasalam@gmail.com>
2022-02-22 17:45:05 +0500

    implemented active/inactive sensors tab

--- src/components/activeSensors.jsx
+++ src/components/activeSensors.jsx
@@ -1,95 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
 	Table, Layout,
 } from 'antd';
+import { getSensors } from '../utils/api';
+import { activeColumns } from '../utils/constants';
 
 const {
 	Header,
 } = Layout;
-const columns = [
-	{
-		title: 'Sensor Id',
-		dataIndex: 'sensorId',
-		key: 'sensorId',
-		render: (text) => <a href={text}>{text}</a>,
-	},
-	{
-		title: 'Asset Id',
-		dataIndex: 'assetId',
-		key: 'assetId',
-	},
-	{
-		title: 'Zone',
-		dataIndex: 'zone',
-		key: 'zone',
-	},
-	{
-		title: 'Arrival Time',
-		key: 'arrivalTime',
-		dataIndex: 'arrivalTime',
-		//   render: tags => (
-		// 	<>
-		// 	  {tags.map(tag => {
-		// 		let color = tag.length > 5 ? 'geekblue' : 'green';
-		// 		if (tag === 'loser') {
-	// 		  color = 'volcano';
-	// 		}
-	// 		return (
-	// 		  <Tag color={color} key={tag}>
-	// 			{tag.toUpperCase()}
-	// 		  </Tag>
-	// 		);
-	// 	  })}
-	// 	</>
-		//   ),
-	},
-	{
-		title: 'Time Spent',
-		key: 'timeSpent',
-		dataIndex: 'timeSpent',
-	//   render: (text, record) => (
-	// 	<Space size="middle">
-	// 	  <a>Invite {record.name}</a>
-	// 	  <a>Delete</a>
-	// 	</Space>
-	//   ),
-	},
-];
 
-const data = [
-	{
-		key: '1',
-		sensorId: '001',
-		assetId: 32,
-		zone: 'B',
-		arrivalTime: '12:34:45',
-		timeSpent: '2 Hrs',
-	},
-	{
-		key: '2',
-		sensorId: '002',
-		assetId: 42,
-		zone: 'C',
-		arrivalTime: '12:34:45',
-		timeSpent: '1 Hrs',
-	},
-	{
-		key: '3',
-		sensorId: '234',
-		assetId: 12,
-		zone: 'B',
-		arrivalTime: '12:34:45',
-		timeSpent: '2 Hrs',
-	},
-];
+//   render: tags => (
+// 	<>
+// 	  {tags.map(tag => {
+// 		let color = tag.length > 5 ? 'geekblue' : 'green';
+// 		if (tag === 'loser') {
+// 		  color = 'volcano';
+// 		}
+// 		return (
+// 		  <Tag color={color} key={tag}>
+// 			{tag.toUpperCase()}
+// 		  </Tag>
+// 		);
+// 	  })}
+// 	</>
+//   ),
+
+//   render: (text, record) => (
+// 	<Space size="middle">
+// 	  <a>Invite {record.name}</a>
+// 	  <a>Delete</a>
+// 	</Space>
+//   ),
 
 function ActiveSensors() {
+	const [data, setData] = useState();
+	useEffect(() => {
+		getSensors().then((res) => {
+			if (!res) return;
+			const activeSensors = res?.filter((value) => value.asset !== null);
+			if (activeSensors) {
+				console.log(activeSensors);
+				setData(activeSensors);
+			}
+		});
+	}, []);
 	return (
   <div>
     <Header className="site-layout-background" style={{ padding: 0 }}>
       <h1>Currently Active Sensors</h1>
     </Header>
-    <Table columns={columns} dataSource={data} />
+    <Table columns={activeColumns} dataSource={data} />
   </div>
 	);
 }
--- src/components/inactiveSensors.jsx
+++ src/components/inactiveSensors.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {
+	Table, Layout,
+} from 'antd';
+import { getSensors } from '../utils/api';
+import { inActiveColumns } from '../utils/constants';
+
+const {
+	Header,
+} = Layout;
 
 function InactiveSensors() {
-	return <h1>Currently InActive Sensors</h1>;
+	const [data, setData] = useState();
+	useEffect(() => {
+		getSensors().then((res) => {
+			if (!res) return;
+			const inActiveSensors = res?.filter((value) => value.asset === null);
+			if (inActiveSensors) {
+				console.log(inActiveSensors);
+				setData(inActiveSensors);
+			}
+		});
+	}, []);
+	return (
+  <div>
+    <Header className="site-layout-background" style={{ padding: 0 }}>
+      <h1>Currently InActive Sensors</h1>
+    </Header>
+    <Table columns={inActiveColumns} dataSource={data} />
+  </div>
+	);
 }
 
 export default InactiveSensors;
