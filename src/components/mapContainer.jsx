2d51dbf4f0f073d7b5f913af1cde943a375c0de7
Zunaira Salam <zunairasalam@gmail.com>
2022-03-07 13:55:32 +0500

    added background images & icons

--- src/components/mapContainer.jsx
+++ src/components/mapContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { GoogleMap, Marker } from '@react-google-maps/api';
 import { locations } from '../utils/constants';
+import image from '../container_icon.png';
 
 const { google } = window;
-const locationPin = 'https://cdn-icons-png.flaticon.com/512/1255/1255714.png';
-function MapContainer() {
+// const locationPin = 'https://cdn-icons-png.flaticon.com/512/1255/1255714.png';
+function MapContainer(sensorsDetailsProps) {
 	const mapStyles = {
 		height: '100vh',
 		width: '100%',
@@ -13,9 +14,8 @@ function MapContainer() {
 	const defaultCenter = {
 		lat: 25.109895082618095, lng: 62.342519825122366,
 	};
-
+	console.log(sensorsDetailsProps);
 	return (
-
   <GoogleMap
     mapContainerStyle={mapStyles}
     zoom={15}
@@ -29,7 +29,7 @@ function MapContainer() {
           key={item.name}
           position={item.location}
           icon={{
-          	url: locationPin,
+          	url: image,
           	anchor: new google.maps.Point(17, 46),
 
           	scaledSize: new google.maps.Size(37, 37),
